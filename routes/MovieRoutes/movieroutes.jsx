7af319015279da8489c86cc7ac3e71dc304c147d
Devke <chriseke13@gm>
2022-11-05 10:52:06 -0500

    Added a route to get movie by ID

--- routes/MovieRoutes/movieroutes.jsx
+++ routes/MovieRoutes/movieroutes.jsx
@@ -16,6 +16,18 @@ module.exports = (router) => {
             })
     })
 
+    router.get('/movie/:movieID',
+    // passport.authenticate('jwt'),
+    (req, res) => {
+        Movies.findById({_id: req.params.movieID})
+        .then((movie) => {
+            res.status(200).send({message: 'Movie successfully retrieved', status: 'success', movie: movie})
+        })
+        .catch((err) => {
+            res.status(500).send({message: 'There was a problem finding that movie', status: 'fail', error: err})
+        })
+    })
+
     // GETS A SINGLE RANDOM MOVIE
     router.get('/movie', 
     // passport.authenticate('jwt'), 
