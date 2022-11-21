c7909ad974a4f5f2355c09cfb4232937d6f8241f
Devke <chriseke13@gm>
2022-11-21 09:36:02 -0600

    Update error message on creating user account

--- routes/UserRoutes/createAccount.jsx
+++ routes/UserRoutes/createAccount.jsx
@@ -26,7 +26,7 @@ module.exports = (router) => {
         Users.findOne({email: req.body.email})
         .then((user) => {
             if (user) {
-               return res.status(400).send('Email already exists')
+               return res.status(400).send({message: 'Email already exists', status: 'success'})
             } else {
                 let initArray = [];
                 let firstAccount = {};
